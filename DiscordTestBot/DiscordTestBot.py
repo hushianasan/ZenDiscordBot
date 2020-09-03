3a896fcf53898c52326859a4096584d585df4f37
ringome <rinngoame.coc@gmail.com>
2020-09-03 21:27:00 +0900

    プロンプト画面を見やすくした

--- DiscordTestBot/DiscordTestBot.py
+++ DiscordTestBot/DiscordTestBot.py
@@ -5,10 +5,6 @@ import discord
 import requests
 
 
-import aiohttp
-import asyncio
-
-
 # Botのアクセストークン
 TOKEN = 'NzQ1NDk4ODg0NDI3NjEyMjMw.Xzyp8A.mg07XDO9AmhV6MjIU4Blqx7M1l4'
 
@@ -21,24 +17,22 @@ def login_check():
     async def on_ready():
         print("Discordにログインしました")
 
-def Discord_Message():
-    async with aiohttp.ClientSession() as session:
 
-
-'''def Discord_Message():    
+def Discord_Message():    
     response = requests.get('https://api.github.com/zen')
     soup = response.text
-    return soup'''
+    return soup
 
 def command_judge():
     @client.event
     async def on_message(message):
         if message.author.bot:
-            print("Bot送信")
+            print("Bot送信 : ", end="")
+            print(Discord_Message())
             return
         if message.content == '/810':
-            print("User送信")
-            print(Discord_Message())
+            print("User送信 : ", end="")
+            print(message.content)
             await message.channel.send(Discord_Message())
         
 ############################################
