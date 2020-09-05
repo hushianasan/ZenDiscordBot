d34cc5b6ee9de19785802774b47511b8a9d4e0a6
ringome <rinngoame.coc@gmail.com>
2020-09-05 15:03:59 +0900

    2020年9月5日ぬるさんに見せる進捗。

--- DiscordTestBot/DiscordTestBot.py
+++ DiscordTestBot/DiscordTestBot.py
@@ -10,31 +10,36 @@ import asyncio
 
 
 # Botのアクセストークン
-TOKEN = 'NzQ1NDk4ODg0NDI3NjEyMjMw.Xzyp8A.SSKoo61xXapSt2Ldsrie-eTgmpE'
+TOKEN = 'NzQ1NDk4ODg0NDI3NjEyMjMw.Xzyp8A.mg07XDO9AmhV6MjIU4Blqx7M1l4'
 
 client = discord.Client()
 
 ############################################
 
+#Discordのログイン確認
 def login_check():
     @client.event
     async def on_ready():
         print("Discordにログインしました")
 
-
+#requestsでZenBotのGet
 '''def Discord_Message():    
     response = requests.get('https://api.github.com/zen')
     soup = response.text
     return soup'''
 
+#aiohttpでZenBotのGet
 async def Discord_Message():
     async with aiohttp.ClientSession() as session:
         async with session.get('https://api.github.com/zen') as resp:
-            #soup = await resp.text()
-            loop = asyncio.get_event_loop()
-            soup = loop.run_until_complete(Discord_Message())
+            print(await resp.text())
+            soup = await resp.text()
             return soup
+loop = asyncio.get_event_loop()
+loop.run_until_complete(Discord_Message())
 
+
+#送信のジャッジ
 def command_judge():
     @client.event
     async def on_message(message):
@@ -45,6 +50,8 @@ def command_judge():
         if message.content == '/810':
             print("User送信 : ", end="")
             print(message.content)
+
+            print(Discord_Message())
             await message.channel.send(Discord_Message())
         
 ############################################
